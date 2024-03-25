3a178730558731464cc768bebc6955e6b7072ed3
rakshit <rakshitambi7a@gmail.com>
2024-03-25 23:32:03 +0530

    solved the initial 8 on my own

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,24 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function sum(array){
+  return array.reduce((sum,number) => sum+=number, 0);
+}
+function multiply(array){
+  return array.reduce((product,number)=> product*=number, 1);
+}
+function power(a,b){
+  return Math.pow(a,b);
+}
+function factorial(a){
+  if(a == 0){
+    return 1;
+  }
+  return a*factorial(a-1);
+}
 // Do not edit below this line
 module.exports = {
   add,
